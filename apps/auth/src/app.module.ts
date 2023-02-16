2bb6a60f5013d8096de81d4055b862d8b319355b
LocNguyen2000 <lolkun2000@gmail.com>
2023-02-16 15:56:43 +0700

    vue admin setup

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DbConnection } from './common/const';
 import { EnvModule, EnvService } from '@nhl/env';
